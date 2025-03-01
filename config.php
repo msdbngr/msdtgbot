1dc1978b6f49474a5950b792e1ae1005c01c6797
Masoud Banagar <44742912+msdbngr@users.noreply.github.com>
2025-03-01 05:11:56 +0330

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// تنظیمات پایه
-define('BOT_TOKEN', 'mytoken');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'blfusngm_masoudbbot_user');
-define('DB_PASS', 'F&W__Z0aK9PF');
-define('DB_NAME', 'blfusngm_masoudbbot_db');
-?>
